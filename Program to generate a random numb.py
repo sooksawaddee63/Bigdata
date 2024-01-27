b656a3233f7174624d7563264e600f4d083fc0dd
sooksawaddee63 <65892124+sooksawaddee63@users.noreply.github.com>
2024-01-27 14:42:36 +0700

    Update #2 Program to generate a random numb.py

--- Program to generate a random numb.py
+++ Program to generate a random numb.py
@@ -1,6 +1,6 @@
 # Program to generate a random number between 0 and 9
 
-# importing the random module - update#1
+# importing the random module - update#2
 import random
 
 print(random.randint(0,9))
